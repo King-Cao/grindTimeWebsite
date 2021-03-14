6f94e9f7db2cd2c4a94c3d925ac85c7e7d5129da
David <dcao@ualberta.ca>
2021-03-13 22:05:12 -0700

    made a rudimentary main page similar to portfolio style

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-let password; 
+/*let password; 
 let pass1 = "theGrindingElite123"
+let adminPassword = "adminpassword"
 
 password = prompt("Please enter the password to view this page!");
 
-if (password == pass1) {
+if (password == pass1 || adminPassword) {
     alert("Password Correct! Click OK to enter!");
 } else {
     window.location = "http://www.dhmo.org/"
 }
+*/
